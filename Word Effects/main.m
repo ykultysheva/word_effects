ae7c5cc6d741ec83d2f01af3724168ce0207a895
Yana Kultysheva <ykultysheva@gmail.com>
2016-06-28 11:44:11 -0400

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Yana Kultysheva on 2016-06-28.
+//  Copyright © 2016 Yana Kultysheva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
